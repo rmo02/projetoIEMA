b7c707cd053b9c8962d03d55c89c170d2493facb
Alexandra Silva <alexandra.silva@mirante.com.br>
2023-08-25 15:33:59 -0300

    fazendo cabeçalho

--- src/screens/Contatos/index.js
+++ src/screens/Contatos/index.js
@@ -1,9 +1,33 @@
-import { Container } from "./styles";
+import { BotaoAdd, BotaoVoltar, Container, ContainerButtom, Header, Perfil, Pesquisa, Text } from "./styles";
+import nario from "../../../assets/nario.png"
+import { Ionicons } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
+import { TextInput } from "react-native";
 
-export function Contatos () {
+
+
+export function Contatos() {
     return (
         <Container>
-            <Text>Contatos</Text>
+            <Header>
+                <ContainerButtom>
+                    <BotaoVoltar>
+                        <AntDesign name="arrowleft" size={24} color="white" />
+                    </BotaoVoltar>
+
+                    <BotaoAdd>
+                        <Ionicons name="add" size={24} color="white" />
+                    </BotaoAdd>
+
+
+                </ContainerButtom>
+                    <Pesquisa
+                        placeholder="pesquisar"
+                    />
+            </Header>
+            <Perfil
+                source={nario}
+            />
         </Container>
     );
 }
--- src/screens/Contatos/styles.js
+++ src/screens/Contatos/styles.js
@@ -2,9 +2,56 @@ import styled from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
+  padding: 20px;
+  background-color:${(props) => props.theme["FUNDO"]};
 `;
 
 export const Text = styled.Text`
   font-size: 24px;
   color: black;
 `;
+
+export const Perfil =styled.Image`
+width: 100px;
+height:120px;
+`;
+
+export const Header = styled.View`
+width:'100%';                               
+flex-direction: row;
+padding-top: 10px;
+padding-bottom: 10px;
+justify-content: space-between;
+`;
+
+export const ContainerButtom=styled.View`
+flex-direction: row;
+
+`;
+
+export const BotaoVoltar=styled.TouchableOpacity`
+height: 50px;
+width: 50px;
+border-radius: 50px;
+justify-content: center;
+align-items: center;
+margin-right:10px;
+background-color: ${(props) => props.theme["BLUE_700"]};
+`;
+
+export const BotaoAdd=styled.TouchableOpacity`
+height: 50px;
+width: 50px;
+border-radius: 50px;
+justify-content: center;
+align-items: center;
+background-color: ${(props) => props.theme["BLUE_700"]};
+`;
+
+export const Pesquisa = styled.TextInput`
+width: 330px;
+border-radius: 50px;
+padding-left: 10px;
+background-color: ${(props) => props.theme["WHITE"]};
+`;
+
